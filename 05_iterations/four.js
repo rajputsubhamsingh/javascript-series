2e3df1720debef3a669f7c09cc0d117c66c88763
rajputsubhamsingh <skrsinghkrsingh.me2020@rceroorkee.in>
2024-02-01 18:11:33 +0530

    iteration

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -19,12 +19,12 @@ for (const key in programming) {
 // iteration not possiable
 
 
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
 
-// for (const key in map) {
-//     console.log(key);
-// }
+for (const key in map) {
+    console.log(key);
+}
